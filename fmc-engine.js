2b7c145cbb2664566aa7989db7cc80a42af4c908
Alex Morrison <ajm293@bath.ac.uk>
2024-02-01 18:55:34 +0000

    Partially implement parser and tree

--- fmc-engine.js
+++ fmc-engine.js
@@ -1,5 +1,102 @@
-function parse() {
+const ex7 = "in<x>.in<y>.([y].[x].+ ; <p>.[p]out)";
 
+const separator = /\(|\)|\.|\[|\]|\;/;
+const alphanum = /[a-z0-9]|_/i;
+const digit = /^\d+$/;
+const varID = /[a-z]+$/;
+const jmpID = /[A-Z][A-Za-z]+$/;
+
+function parse(tokenStream) {
+    "use strict";
+    let input = tokenStream;
+    let index = 0;
+    return parse();
+    function parse() {
+        let result = term();
+        return result;
+    }
+    function term () {
+        let sym = nextToken();
+        console.log(`Processing token ${sym} as term`);
+        if (sym == '(') {
+            // loopback and check
+        }
+        if (sym == '[') {
+            // push action
+        } else if (sym == '<') {
+            // pop action on lambda stack
+            return popAction("");
+        } else if (sym == ';') {
+            // jump action
+        } else if (sym == '^') {
+            // loop
+        } else if (alphanum.test(sym)) {
+            // jump if Capitalised or digit
+            // variable or pop location if lowercase
+            if (digit.test(sym) || jmpID.test(sym)) {
+                // jump
+            } else if (varID.test(sym)) {
+                if (lookAhead() == '<') {
+                    // pop action on location
+                    nextToken();
+                    return popAction(sym);
+                } else {
+                    return variable(sym);
+                }
+            }
+        } else if (sym == '\0') {
+            console.log("We have reached the end");
+            return variable("");
+        }
+        // How do we handle brackets?
+    }
+    function pushAction() {
+        // [TERM]loc.TERM
+        // nextToken() should be the start of a TERM
+        
+    }
+    function popAction(location) {
+        // loc<var>.TERM
+        // nextToken() should be a variable
+        let a = location;
+        let x = variable(nextToken());
+        let rangle = nextToken();
+        let dot = nextToken();
+        if (rangle == '>' && dot == '.') {
+            let m = term();
+            return new L(a, x, m);
+        }
+    }
+    function jumpAction() {
+        // TERM ; jump -> TERM
+        // nextToken() should be a Jump
+    }
+    function loop() {
+        // TERM^jump
+        // nextToken() should be a Jump
+    }
+    function variable(x) { // Terminal
+        return new V(x);
+    }
+    function jump() { // Terminal
+
+    }
+    function nextToken() {
+        if (index >= input.length) {
+            return "\0";
+        }
+        return input[index++];
+    }
+    function lookAhead() {
+        if (index+1 >= input.length) {
+            return "\0";
+        }
+        return input[index];
+    }
+    function error() {
+        console.error(`Error at token ${index}`);
+        throw new Error("Parsing error");
+    }
 }
 
 function step() {
@@ -14,8 +111,6 @@ function init() {
     
 }
 
-const ex7 = "in<x>.in<y>.([y].[x].+ ; <p>.[p]out)"
-
 function tokenise(input) {
     input = input.split(/\s/).join("");
     let index = 0;
@@ -23,20 +118,20 @@ function tokenise(input) {
     let tok = '';
     do {
         curr = input[index++];
-        if (/\(|\)|\.|\[|\]|\;/.test(curr)) { // Separator characters
+        if (separator.test(curr)) { // Separator characters
             if (tok != '') {
                 tokenStream.push(tok);
                 tok = '';
             }
             tokenStream.push(curr);
-        } else if (/[a-z0-9]|_/ig.test(curr)) { // Alphanumeric identifiers
-            if (!(/[a-z0-9]|_/ig.test(tok)) && tok != '') {
+        } else if (alphanum.test(curr)) { // Alphanumeric identifiers
+            if (!(alphanum.test(tok)) && tok != '') {
                 tokenStream.push(tok);
                 tok = '';
             }
             tok += curr;
-        } else { // Non-alphanumeric cohesive features <, >, and ->
-            if (/[a-z0-9]|_/ig.test(tok) && tok != '') {
+        } else { // Non-alphanumeric cohesive features: operators, <, >, ^, and ->
+            if (alphanum.test(tok) && tok != '') {
                 tokenStream.push(tok);
                 tok = '';
             }
---
+++ tree.js
@@ -0,0 +1,50 @@
+class V { // variable (terminal)
+    constructor(value) {
+        this.value = value;
+    }
+
+    toString() {
+        return (`V(${this.value})`);
+    }
+}
+
+class L { // pop action (lambda-abstraction)
+    constructor(loc, variable, term) {
+        this.loc = loc;
+        this.variable = variable;
+        this.term = term;
+    }
+
+    toString() {
+        return (`L(${this.loc}, ${this.variable}, ${this.term})`)
+    }
+}
+
+class A { // push action (application)
+    constructor(loc, pushTerm, term) {
+        this.loc = loc;
+        this.pushTerm = pushTerm;
+        this.term = term;
+    }
+}
+
+class J { // jump (terminal)
+    constructor(value) {
+        this.value = value;
+    }
+}
+
+class S { // jump action (sequence)
+    constructor(lTerm ,jmp, rTerm) {
+        this.lTerm = lTerm;
+        this.jmp = jmp;
+        this.rTerm = rTerm;
+    }
+}
+
+class R { // loop (recurse)
+    constructor(term, jmp) {
+        this.term = term;
+        this.jmp = jmp;
+    }
+}
